8e874f4c52e570c8292220c8f29c79578ff399b2
19980ductri <67219928+19980ductri@users.noreply.github.com>
2023-11-27 00:02:18 +0700

    Shaman added

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -61,7 +61,7 @@ protected:
 	virtual void BeginPlay() override;
 
 	virtual void InitAbilityActorInfo();
-	UPROPERTY(EditAnywhere, Category= "Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category= "Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 	UPROPERTY()
