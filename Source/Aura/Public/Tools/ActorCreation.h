1216996b87b7a3dccf54fa73a31d22d2162db5aa
19980ductri <67219928+19980ductri@users.noreply.github.com>
2023-12-06 03:30:11 +0700

    tool init

---
+++ Source/Aura/Public/Tools/ActorCreation.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class AURA_API ActorCreation
+{
+public:
+	ActorCreation();
+
+	
+};
